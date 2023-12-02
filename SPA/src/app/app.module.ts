3651ac20ac694d89ff6956c470da58a39cc17ace
Mohamed Farid <mohamedf4@Mobiquityinc.com>
2023-12-02 12:45:47 -0500

    Init UI layout

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -6,20 +6,55 @@ import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponentComponent } from './demo-component/demo-component.component';
+import { SidebarComponent, WorkspaceComponent } from './shared/index';
+import { RouterModule, PreloadAllModules, Route } from '@angular/router';
+import {
+  HomeComponent,
+  ListComponent,
+  DetailComponent,
+  EntryComponent
+} from './components/index';
+import {
+  BladerModule,
+  BladeRegistry,
+  BladeMetaData
+} from './blader/index';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponentComponent
+    DemoComponentComponent,
+    SidebarComponent,
+    WorkspaceComponent,
+    HomeComponent,
+    EntryComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MonacoEditorModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    BladerModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'list', component: ListComponent },
+      { path: 'detail', component: DetailComponent },
+      { path: 'lazy', loadChildren: () => import('./lazy/lazy.module').then(m => m.LazyModule) }
+    ], { preloadingStrategy: PreloadAllModules })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  public constructor(
+    private _bladeRegistry: BladeRegistry
+  ) {
+    this._bladeRegistry.register(new BladeMetaData('entry', EntryComponent));
+    this._bladeRegistry.register(new BladeMetaData('list', ListComponent));
+    this._bladeRegistry.register(new BladeMetaData('detail', DetailComponent));
+  }
+}
